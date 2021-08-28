5a06e914303c6b577f8d4601bc948d93de7110fb
Joshua D Wood <joshuawd190@gmail.com>
2021-08-28 06:30:23 -0500

    update server to use apollo server

--- server/server.js
+++ server/server.js
@@ -5,9 +5,22 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+async function startup() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startup();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,8 +30,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
